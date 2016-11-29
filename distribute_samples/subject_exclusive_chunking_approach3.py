886981d9aea5416305f07d5a8168c4139b342bc4
narita.pandhe <narita.pandhe@gmail.com>
2016-11-29 03:18:28 -0500

    Modified subject_exclusive_chunking script. It now creates chunks with equal gender distributions

--- distribute_samples/subject_exclusive_chunking_approach3.py
+++ distribute_samples/subject_exclusive_chunking_approach3.py
@@ -10,7 +10,8 @@ from ast import literal_eval as make_tuple
 from operator import itemgetter
 from collections import Counter
 import math
-
+import cPickle
+import itertools
 '''
 1. Read each fold for each age group
 2. Add counts of each age group
@@ -73,7 +74,7 @@ def load_pickles(name,no_of_age_groups = 8):
 		h=pickle.load(f)
 
 	return a+b+c+d+e+f1+g+h
-
+	
 def get_age_range_id(age_tuple):
 	age_ranges = [(0,2),(4,6),(8,13),(15,20),(25,32),(38,43),(48,53),(60,100)]
 	diff_tuple = []
@@ -143,34 +144,28 @@ def distribute_data(male_age_group_counters_across_folds,female_age_group_counte
 	print ("\nIn distributed data....")
 
 	fold_names = ['fold_0_data','fold_1_data','fold_2_data','fold_3_data']	
-	male_pickle_file_path_prefix = '/home/narita/Documents/pythonworkspace/data-science-practicum/gender-age-classification/gender_based_data/male/'
-	female_pickle_file_path_prefix = '/home/narita/Documents/pythonworkspace/data-science-practicum/gender-age-classification/gender_based_data/female/'
 	age_range_ids = [0,1,2,3,4,5,6,7]
 
-	for a in age_range_ids:
-	
-		male_chunk_size = int(math.ceil(male_age_group_counters_across_folds[a]))
-		female_chunk_size = int(math.ceil(female_age_group_counters_across_folds[a]))
+
+
+	for ari in age_range_ids:
+		male_chunk_size = int(math.ceil(male_age_group_counters_across_folds[ari]))
+		female_chunk_size = int(math.ceil(female_age_group_counters_across_folds[ari]))
 
 		male_overflow = []
-		female_overflow = []
-
 		male_folds_needing_adjustment = []
-		female_folds_needing_adjustment = []
+
+		malelist0 = []
+		malelist1 = []
+		malelist2 = []
+		malelist3 = []
 		
-		fold_i=0
+
 		for fold in fold_names:
-			male_age_group_data = []
-			female_age_group_data = []
-
-			'''
-				male_fold_i's data will be in csv/fold_i
-				Similarly for female
-
-			'''
-			fold_df = pd.read_csv('/home/narita/Documents/pythonworkspace/data-science-practicum/gender-age-classification/data/csvs/'+fold+'.csv')
 			male_rec_count = 0
-			female_rec_count = 0
+			male_age_group_data = []
+		
+			fold_df = pd.read_csv('/home/narita/Documents/pythonworkspace/data-science-practicum/gender-age-classification/data/csvs/'+fold+'.csv')
 
 			for index, row in fold_df.iterrows():
 
@@ -178,85 +173,98 @@ def distribute_data(male_age_group_counters_across_folds,female_age_group_counte
 				age = row['age']
 				gender = row['gender']
 				yaw_angle = row['fiducial_yaw_angle']
-				
-				
-				if ((gender!='u') and (gender!='Nan') and (age!='None') and (gender!=' ') and (age!=' ') and (yaw_angle >= -45) and (yaw_angle <= 45)):
+			
+				if ((gender!='u') and (gender!='Nan') and (age!='None') and (gender!=' ') and (age!=' ') and (yaw_angle >= -45) and (yaw_angle <= 45)): 
+					
 					age_tuple = make_tuple(age)
 					age_id = get_age_range_id(age_tuple)
-					
-					if(age_id == a):
 
-						d = {	
+					if(age_id == ari): 
+
+						dict = {	
 								'user_id': row['user_id'], 'original_image': row['original_image'], 
 								'face_id': row['face_id'], 'original_age': age,
 								'original_gender': gender, 'yaw_angle': yaw_angle,
 								'original_fold_name': fold, 'age_id': age_id
 
-							}
+							   }
+
 						if((gender == 'm')):
 							if(male_rec_count < male_chunk_size):
 								male_rec_count+=1
-								male_age_group_data.append(d)
+								male_age_group_data.append(dict)
 							else:
-								male_overflow.append(d)
+								male_overflow.append(dict)	
 
-						elif((gender == 'f')):
-							if (female_rec_count < female_chunk_size):
-								female_rec_count+=1
-								female_age_group_data.append(d)
-							else:
-								female_overflow.append(d)
+					
 
-			'''
-			Dump the records of new fold
-			'''
-			if(male_rec_count != male_chunk_size):
-				male_folds_needing_adjustment.append({'fold_name':fold_i,'adjustment_count_needed':(abs(male_chunk_size-male_rec_count))})
-
-			save_path='/home/narita/Documents/pythonworkspace/data-science-practicum/gender-age-classification/new_distributed_data_subject_exclusive/male/'
-			with open(save_path+'male_fold_'+str(fold_i) + '.pkl', 'a') as f:
-				pickle.dump(male_age_group_data, f, pickle.HIGHEST_PROTOCOL)
-
-
-			if(female_rec_count != female_chunk_size):
-				female_folds_needing_adjustment.append({'fold_name':fold_i,'adjustment_count_needed':(abs(female_chunk_size-female_rec_count))})
-
-			save_path='/home/narita/Documents/pythonworkspace/data-science-practicum/gender-age-classification/new_distributed_data_subject_exclusive/female/'
-			with open(save_path+'female_fold_'+str(fold_i) + '.pkl', 'a') as f:
-				pickle.dump(female_age_group_data, f, pickle.HIGHEST_PROTOCOL)
-
-			print len(male_overflow)
-			print len(female_overflow)
-			fold_i+=1			
+				
 		
-		'''
-		Before exiting the age_group loop
-		redistribute overflow into existing
+			
+			if(male_rec_count != male_chunk_size):
+				male_folds_needing_adjustment.append({'fold_name':fold,'adjustment_count_needed':(abs(male_chunk_size-male_rec_count))})
 
+			if (fold == 'fold_0_data'):
+				malelist0.append(male_age_group_data)
+			if (fold == 'fold_1_data'):
+				malelist1.append(male_age_group_data)
+			if (fold == 'fold_2_data'):
+				malelist2.append(male_age_group_data)
+			if (fold == 'fold_3_data'):
+				malelist3.append(male_age_group_data)
+
+			'''	
+			save_path='/home/narita/Documents/pythonworkspace/data-science-practicum/gender-age-classification/new_distributed_data_subject_exclusive/male/'
+			with open(save_path+'male_'+fold + '.json', 'a') as f:
+				print('Dumping data to: %s' % (save_path+'male_'+fold + '.json'))
+				#cPickle.dump(male_age_group_data, f, pickle.HIGHEST_PROTOCOL)
+				json.dump(male_age_group_data,f)
+			'''
+
+		
 		start = 0
 		for adj in male_folds_needing_adjustment:
 			remaining_data = male_overflow[start:start+adj['adjustment_count_needed']]
-			print type(remaining_data)
-			save_path='/home/narita/Documents/pythonworkspace/data-science-practicum/gender-age-classification/new_distributed_data_subject_exclusive/male/'
-			with open(save_path+'male_fold_'+str(adj['fold_name']) + '.pkl', 'a') as f:
-				pickle.dump(remaining_data, f, pickle.HIGHEST_PROTOCOL)
-			start = adj['adjustment_count_needed']+1
-
-
-		start = 0
-		for adj in female_folds_needing_adjustment:
-			remaining_data = female_overflow[start:start+adj['adjustment_count_needed']]
-			save_path='/home/narita/Documents/pythonworkspace/data-science-practicum/gender-age-classification/new_distributed_data_subject_exclusive/female/'
-			with open(save_path+'female_fold_'+str(adj['fold_name']) + '.pkl', 'a') as f:
-				pickle.dump(remaining_data, f, pickle.HIGHEST_PROTOCOL)
-			start = adj['adjustment_count_needed']+1
-		
-		'''
+			if (adj['fold_name'] == 'fold_0_data'):
+				malelist0.append(remaining_data)
+			if (adj['fold_name'] == 'fold_1_data'):
+				malelist1.append(remaining_data)
+			if (adj['fold_name'] == 'fold_2_data'):
+				malelist2.append(remaining_data)
+			if (adj['fold_name'] == 'fold_3_data'):
+				malelist3.append(remaining_data)
 	
-		print a
+			start = adj['adjustment_count_needed']
+		
+		save_path='/home/narita/Documents/pythonworkspace/data-science-practicum/gender-age-classification/new_distributed_data_subject_exclusive/male/'
+		with open(save_path+'male_fold_0_data' + '.pkl', 'a') as f:
+				pickle.dump(list0, f, pickle.HIGHEST_PROTOCOL)
+			
+
+		with open(save_path+'male_fold_1_data' + '.pkl', 'a') as f:
+				pickle.dump(list1, f, pickle.HIGHEST_PROTOCOL)
+
+		with open(save_path+'male_fold_2_data' + '.pkl', 'a') as f:
+				pickle.dump(list2, f, pickle.HIGHEST_PROTOCOL)
+
+		with open(save_path+'male_fold_3_data' + '.pkl', 'a') as f:
+				pickle.dump(list3, f, pickle.HIGHEST_PROTOCOL)
+			
+		
+			
+		'''
+			if(male_rec_count != male_chunk_size):
+				male_folds_needing_adjustment.append({'fold_name':fold_i,'adjustment_count_needed':(abs(male_chunk_size-male_rec_count))})
+		
+		print ("Age group: %i" % ari)							
+		print ("Before exiting, size: %i\n" % len(male_overflow))
+		'''						
+						
+
+		
 
 def read_new_folds():
-	fold_names = ['0','1','2','3']	
+	fold_names = ['fold_0_data','fold_1_data','fold_2_data','fold_3_data']	
 	
 	male_pickle_file_path_prefix = '/home/narita/Documents/pythonworkspace/data-science-practicum/gender-age-classification/new_distributed_data_subject_exclusive/male/'
 	female_pickle_file_path_prefix = '/home/narita/Documents/pythonworkspace/data-science-practicum/gender-age-classification/new_distributed_data_subject_exclusive/female/'
@@ -270,9 +278,11 @@ def read_new_folds():
 		train_ages = []
 
 		print('Trying to read training fold: %s......' % fold)
-		male_data = load_pickles(male_pickle_file_path_prefix+'male_fold_'+fold)
-		male_df = pd.DataFrame(male_data)
-
+		male_data = load_pickles(male_pickle_file_path_prefix+'male_'+fold)
+		merged = list(itertools.chain(*male_data))
+		print len(merged)
+		male_df = pd.DataFrame(merged)
+		
 		male_ages = np.array(male_df['age_id'])
 		male_age_counter = Counter(male_ages)
 		male_age_counter_dict = dict(male_age_counter)
@@ -282,6 +292,7 @@ def read_new_folds():
 		for i in range(8):
 			if i in male_age_counter_dict:
 				male_age_group_counters_across_folds[i] += male_age_counter_dict[i]
+		'''		
 
 		female_data = load_pickles(female_pickle_file_path_prefix+'female_fold_'+fold)
 		female_df = pd.DataFrame(female_data)
@@ -295,6 +306,7 @@ def read_new_folds():
 		for i in range(8):
 			if i in female_age_counter_dict:
 				female_age_group_counters_across_folds[i] += female_age_counter_dict[i]
+		'''
 		print(' ')
 		
 
